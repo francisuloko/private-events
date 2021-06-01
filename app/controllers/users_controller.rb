adc4cdf37870af208d753e9e495cc20595e0c882
francis uloko <francisuloko@yahoo.com>
2021-06-01 18:07:21 +0100

    Use scope to query user attended_events

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def show
     @created_events = current_user.created_events
 
-    @future_events = current_user.attended_events.where('event_date > ?', Date.today).order('event_date ASC')
-    @past_events = current_user.attended_events.where('event_date <  ?', Date.today).order('event_date DESC')
+    @future_events = current_user.attended_events.future
+    @past_events = current_user.attended_events.past
   end
 end
